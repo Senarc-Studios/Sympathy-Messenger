11c7170819df824ce9316fcdeaecd4789b53333f
BenitzCoding <benitzcoding@yahoo.com>
2022-01-27 23:18:24 +0800

    Created register function.

--- index.py
+++ index.py
@@ -17,3 +17,13 @@ def start(Private_Key: str):
             print(f"{value['username']}'s Public Key: {value['public_key']}\n\n")
     time.sleep(5)
     return start(Private_Key)
+
+def register(username: str):
+    data = {
+        "username": username
+    }
+    response = requests.post(constants.API_ENDPOINT + "/register", json=data)
+    if response.status_code == 200:
+        print("[+] Sympathy-Messenger account registered successfully.")
+    else:
+        print("[-] Sympathy-Messenger account not registered successfully.")
